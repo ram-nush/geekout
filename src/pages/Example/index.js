44acd70befb6d66a5f221121e3ab807526a1b6a7
ram-nush <ramu2003@live.com.sg>
2020-12-09 03:49:09 +0000

    govtechfinal

--- src/pages/Example/index.js
+++ src/pages/Example/index.js
@@ -1,7 +1,7 @@
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import Donations from "../components/Donations";
-import Masthead from "../components/Masthead";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import Donations from "../../components/Donations";
+import Masthead from "../../components/Masthead";
 
 /**
  *  @description
